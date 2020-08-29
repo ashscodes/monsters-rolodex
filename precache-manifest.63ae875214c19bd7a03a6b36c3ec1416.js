5c6554b587fc25df858bf5f84dae72da3f263a38
ashscodes <70372500+ashscodes@users.noreply.github.com>
2020-08-29 18:59:48 +0100

    Updates

---
+++ precache-manifest.63ae875214c19bd7a03a6b36c3ec1416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95c29dae611baae1e0d4157c3e71074",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0071d7fe1ad853b08da5",
+    "url": "/monsters-rolodex/static/css/main.592ac919.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0071d7fe1ad853b08da5",
+    "url": "/monsters-rolodex/static/js/main.0c3d21a1.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
